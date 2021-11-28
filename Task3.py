fd8e9c9f051779cac11ed8cee2582bc16284cd84
IrinaLE3 <94915946+IrinaLE3@users.noreply.github.com>
2021-11-28 21:29:12 +0000

    Python week 2

---
+++ Task3.py
@@ -0,0 +1,6 @@
+meal_1 = input("What is your favourite starter? ")
+meal_2 = input("What is your favourite course? ")
+meal_3 = input("What is your favourite dessert? ")
+meal_4 = input("What is your favourite drink? ")
+dishes = meal_1 + ", " + meal_2 + ", " + meal_3 
+print("Your favourite meal is " + dishes + " with a glass of " +  meal_4)
